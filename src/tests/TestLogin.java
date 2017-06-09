4a8f9065cbecd08dc62216e482e0a72532c0d752
tandrara <traghura@outlook.com>
2017-06-09 05:51:05 +0530

    Updated changes in intelij

--- src/tests/TestLogin.java
+++ src/tests/TestLogin.java
@@ -10,27 +10,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestLogin {
-	private WebDriver driver;
-	private static Logger Log = LogManager.getLogger(TestLogin.class.getName());
+    private WebDriver driver;
+    private static Logger Log = LogManager.getLogger(TestLogin.class.getName());
 
-	@Before
-	public void setUp() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\tandrara\\selenium\\chrome-driver\\chromedriver.exe");
-		driver = new ChromeDriver();
-		Log.debug("Web driver initialized");
-	}
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\tandrara\\selenium\\chrome-driver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        Log.debug("Web driver initialized");
+    }
 
-	@After
-	public void tearDown() {
-		driver.quit();
-		Log.debug("Web driver closed");
-	}
+    @After
+    public void tearDown() {
+        driver.quit();
+        Log.debug("Web driver closed");
 
-	@Test
-	public void withValidCredentials() {
-		driver.get("http://the-internet.herokuapp.com/login");
-		driver.findElement(By.id("username")).sendKeys("tomsmith");
-		driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-		driver.findElement(By.cssSelector("button")).click();
-	}
+    }
+
+    @Test
+    public void withValidCredentials() {
+        driver.get("http://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector("button")).click();
+    }
 }
